f7c766f121af14096e0aea5bea77e0267b32a4fd
Tdtpka <22010146@st.phenikaa-uni.edu.vn>
2024-10-10 22:29:22 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -6,25 +6,28 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+
 Future<void> main() async{
+
   //WidgetsBinding
   final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  //GetX local storage
-  await GetStorage.init();
+
   //Gemini API
   await dotenv.load(fileName: ".env");
   Gemini.init(
     apiKey: dotenv.env["GEMINI_API_KEY"]!,
   );
-  runApp(const MyApp());
+
   //Splash until other items Load
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   //initialize Firebase & Authentication Repository
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,)
   .then((FirebaseApp value) => Get.put(AuthenticationRepository()));
+
   FirebaseFirestore.instance.settings = const Settings(
     persistenceEnabled: true,
   );  
